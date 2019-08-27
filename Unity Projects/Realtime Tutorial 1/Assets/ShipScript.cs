58eecd438e84a56703bde320126eecac098341d8
ThomasMGilman <Thomas.Gilman@ymail.com>
2019-08-27 14:12:54 -0400

    first project start, 3d space invaders

---
+++ Unity Projects/Realtime Tutorial 1/Assets/ShipScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipScript : MonoBehaviour
+{
+    public float moveSpeed = 50.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dz = Input.GetAxis("Horizontal");
+        Vector3 pos = transform.position;           //ships position matrix
+        pos.z += dz * (moveSpeed * Time.deltaTime); //update left/right position on z plane
+
+        if(Input.GetButtonDown("Jump"))             //ships firing code
+        {
+            print("pewpew");
+        }
+        
+        transform.position = pos;
+    }
+}
