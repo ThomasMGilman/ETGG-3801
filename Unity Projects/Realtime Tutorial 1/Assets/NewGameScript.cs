68d8d48506cb669c314d2462a0d9aae8f77a7d6b
ThomasMGilman <Thomas.Gilman@ymail.com>
2019-09-11 18:51:57 -0400

    add resume menu option

--- Unity Projects/Realtime Tutorial 1/Assets/NewGameScript.cs
+++ Unity Projects/Realtime Tutorial 1/Assets/NewGameScript.cs
@@ -9,6 +9,7 @@ public class NewGameScript : MonoBehaviour
     // Start is called before the first frame update
     public RawImage Title;
     public Button NewGameOption;
+    public Button ResumeGameOption;
     public Button QuiteGameOption;
 
     public float ButtonGap = 50f;       //Gap between buttons
@@ -18,6 +19,7 @@ public class NewGameScript : MonoBehaviour
     private RectTransform pRTransform;
     private RectTransform TitleRT;
     private RectTransform NewGameRT;
+    private RectTransform ResumeGameRT;
     private RectTransform QuiteRT;
 
     private float maxHeight;
@@ -30,6 +32,7 @@ public class NewGameScript : MonoBehaviour
         pRTransform     = this.GetComponent<RectTransform>();
         TitleRT         = Title.GetComponent<RectTransform>();
         NewGameRT       = NewGameOption.GetComponent<RectTransform>();
+        ResumeGameRT    = ResumeGameOption.GetComponent<RectTransform>();
         QuiteRT         = QuiteGameOption.GetComponent<RectTransform>();
 
         //set buttons to disabled if animated menu
@@ -44,10 +47,12 @@ public class NewGameScript : MonoBehaviour
             NewGameOption.interactable      = true;
             QuiteGameOption.interactable    = true;
         }
+        ResumeGameOption.interactable       = false; //Resume game is disabled by default
 
         //get half the height of the components
         float TitlesHalfHeight      = TitleRT.rect.height / 2;
         float NewGameHalfHeight     = NewGameRT.rect.height / 2;
+        float ResumeGameHalfHeight  = ResumeGameRT.rect.height / 2;
         float QuiteGameHalfHeight   = QuiteRT.rect.height / 2;
 
         maxHeight = pRTransform.rect.height;
@@ -57,15 +62,18 @@ public class NewGameScript : MonoBehaviour
 
         float TitleStartPosHeight;
         float NewGameStartPosHeight;
+        float ResumeGameStartPosHeight;
         float QuiteStartPosHeight;
         if (AnimateMenu) TitleStartPosHeight    = -TitleRT.rect.height;                 //set start pos for Title Image              
         else TitleStartPosHeight                = maxHeight;
         NewGameStartPosHeight                   = TitleStartPosHeight - TitlesHalfHeight - ButtonGap - NewGameHalfHeight;     //set start pos for newGame Button
-        QuiteStartPosHeight                     = NewGameStartPosHeight - NewGameHalfHeight - ButtonGap - QuiteGameHalfHeight;  //set start pos for QuiteGame Button
+        ResumeGameStartPosHeight                = NewGameStartPosHeight - NewGameHalfHeight - ButtonGap - ResumeGameHalfHeight;  //set start pos for ResumeGame Button
+        QuiteStartPosHeight                     = ResumeGameStartPosHeight - NewGameHalfHeight - ButtonGap - QuiteGameHalfHeight;  //set start pos for QuiteGame Button
 
-        TitleRT.position    = new Vector3(maxWidth, TitleStartPosHeight, 0);
-        NewGameRT.position  = new Vector3(maxWidth, NewGameStartPosHeight, 0);
-        QuiteRT.position    = new Vector3(maxWidth, QuiteStartPosHeight, 0);
+        TitleRT.position        = new Vector3(maxWidth, TitleStartPosHeight, 0);
+        NewGameRT.position      = new Vector3(maxWidth, NewGameStartPosHeight, 0);
+        ResumeGameRT.position   = new Vector3(maxWidth, ResumeGameStartPosHeight, 0);
+        QuiteRT.position        = new Vector3(maxWidth, QuiteStartPosHeight, 0);
     }
 
     // Update is called once per frame
@@ -78,15 +86,18 @@ public class NewGameScript : MonoBehaviour
                 float transformVal = 1;//AnimationSpeed * Time.deltaTime; 
                 Vector3 newTitleRTpos       = TitleRT.position;
                 Vector3 newNewGameRTpos     = NewGameRT.position;
+                Vector3 newResumeGameRTpos  = ResumeGameRT.position;
                 Vector3 newQuiteRTpos       = QuiteRT.position;
 
                 newTitleRTpos.y         += transformVal;
                 newNewGameRTpos.y       += transformVal;
+                newResumeGameRTpos.y    += transformVal;
                 newQuiteRTpos.y         += transformVal;
 
-                TitleRT.position    = newTitleRTpos;
-                NewGameRT.position  = newNewGameRTpos;
-                QuiteRT.position    = newQuiteRTpos;
+                TitleRT.position        = newTitleRTpos;
+                NewGameRT.position      = newNewGameRTpos;
+                ResumeGameRT.position   = newResumeGameRTpos;
+                QuiteRT.position        = newQuiteRTpos;
 
                 if (TitleRT.position.y >= alphaFadeStart) //start fading Title Image
                 {
@@ -108,11 +119,17 @@ public class NewGameScript : MonoBehaviour
     public void startGame()
     {
         //print("starting game!");
+        ResumeGameOption.interactable = true;
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 
     public void quitGame()
     {
         Application.Quit();
+    }
+
+    public void resumeGame()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 }
