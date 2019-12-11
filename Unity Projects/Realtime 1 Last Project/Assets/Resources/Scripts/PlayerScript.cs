a1a73eb9cb5e4c4c4bbb56377294d7840669accb
ThomasMGilman <Thomas.Gilman@ymail.com>
2019-12-11 10:15:00 -0500

    added movement

--- Unity Projects/Realtime 1 Last Project/Assets/Resources/Scripts/PlayerScript.cs
+++ Unity Projects/Realtime 1 Last Project/Assets/Resources/Scripts/PlayerScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float moveSpeed;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rb = this.transform.GetComponent<Rigidbody>();
     }
 
     private Vector3 getMoveOffset()
@@ -15,8 +17,8 @@ public class PlayerScript : MonoBehaviour
         Vector3 offset = Vector3.zero;
         offset.x = Input.GetAxis("Horizontal");
         offset.z = Input.GetAxis("Vertical");
-        print("moving: " + offset);
-        return offset;
+        offset = this.transform.TransformDirection(offset);
+        return offset * Time.deltaTime * moveSpeed;
     }
 
     private void move()
@@ -29,6 +31,6 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move();
     }
 }
